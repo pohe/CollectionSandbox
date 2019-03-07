da40cd3a86d57d8069d5692c65910b5dc35334fa
Poul Henriksen <pohe@easj.dk>
2019-03-07 11:01:23 +0100

    updated med List

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Sandbox
 {
@@ -19,6 +20,7 @@ namespace Sandbox
             //Console.WriteLine($"På plads 3 ligger {tal[3]}");
             //int pl = tal[5];
 
+            tal[4] = 0;
             string[] navne = new string[]  {"Vibeke", "Mohammed", "Peter" };
 
             Student [] students = new Student[]{ new Student("Peter"), new Student("Vibeke")};
@@ -59,6 +61,67 @@ namespace Sandbox
                 Console.WriteLine(tal[i]);   
             }
 
+            Console.WriteLine("Udskrivning af navne tabellen vha. for");
+            for (int i = 0; i < navne.Length; i++)
+            {
+                Console.WriteLine($"På index {i} ligger {navne[i]}");
+            }
+            Console.WriteLine("Udskrivning af navne tabellen vha. while");
+
+            int t = 0;
+            while (t<navne.Length)
+            {
+                Console.WriteLine($"På index {t} ligger {navne[t]}");
+                t++;
+            }
+            Console.WriteLine("Udskrivning af navne tabellen vha. foreach");
+
+            int h = 0;
+            foreach (string value in navne)
+            {
+                Console.WriteLine($"På index {h} ligger {value}");
+                h++;
+            }
+
+            //Udskriv alle navne i Students
+            Console.WriteLine("Udskriv alle navne i Students");
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.Navn);
+            }
+
+            //Erklæring af en List
+            List<int> myList = new List<int>();
+            myList.Add(123);
+            myList.Add(56);
+            myList.Add(99);
+            myList.Add(95);
+            myList.Add(19);
+
+            myList.Insert(2,45);
+
+            myList.RemoveAt(3);
+            myList[2] = 77;
+            Console.WriteLine("Udskrivning af Liste vha. for");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine($"På index {i} ligger {myList[i]}");
+            }
+
+            myList.Sort();
+            myList.Reverse();
+
+            Console.WriteLine("Udskrivning af Liste vha. foreach efter sortering");
+            foreach (int value in myList)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine("Udskrivning af Liste vha. for efter sortering ");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine($"På index {i} ligger {myList[i]}");
+            }
 
 
             //Console.WriteLine("Hello world!");
